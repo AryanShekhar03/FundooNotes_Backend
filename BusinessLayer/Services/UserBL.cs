204d1a2b7e7cc00e46f20b59bcb5b1461f48b982
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-02-24 15:22:14 +0530

    [add]Aaryan added User Registration model

---
+++ BusinessLayer/Services/UserBL.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class UserBL : IUserBL
+    {
+        private readonly IUserRL userRL;
+        public UserBL(IUserRL userRL)
+        {
+            this.userRL = userRL;
+        }
+        public User Registration(UserRegModel userRegModel)
+        {
+           try
+           {
+                return userRL.Registration(userRegModel);
+           }
+           catch(Exception)
+           {
+                throw;
+           }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
