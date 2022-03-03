993aec62a4894b9d408c38131b4ce2aa38613e14
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-03-03 23:19:58 +0530

    [add]Aaryan added api's for Notes

--- BusinessLayer/Services/NotesBL.cs
+++ BusinessLayer/Services/NotesBL.cs
@@ -20,6 +20,18 @@ namespace BusinessLayer.Services
 
         }
 
+        public string ArchieveNotes(long NotesID)
+        {
+            try
+            {
+                return this.notesRL.ArchieveNotes(NotesID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public bool CreateNote(NotesModel notesModel, long userId)
         {
             try
@@ -32,29 +44,77 @@ namespace BusinessLayer.Services
             }
         }
 
-        //public bool CreateNotes(NotesModel notesModel ,long UserId)
-        //{
-        //    try
-        //    {
-        //        return this.notesBL.CreateNote(notesModel , UserId);
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
+        public bool DeleteNotes(long NotesID)
+        {
+            try
+            {
+                return this.notesRL.DeleteNotes(NotesID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
-        //public IEnumerable<Notes> GetAllNotes(long userId)
-        //{
-        //    try
-        //    {
-        //        return this.notesBL.GetAllNotes(userId);
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
+        public IEnumerable<Notes> GetAllNotes(long userId)
+        {
+            try
+            {
+                return this.notesRL.GetAllNotes(userId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string Notescolor(long NotesID, string color)
+        {
+            try
+            {
+                return this.notesRL.Notescolor(NotesID,color);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string Pinned(long NotesID)
+        {
+            try
+            {
+                return this.notesRL.Pinned(NotesID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string TrashNotes(long NotesID)
+        {
+            try
+            {
+                return this.notesRL.TrashNotes(NotesID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string UpdateNote(NotesModel notesUpdateModel, long NotesID)
+        {
+            try
+            {
+                return notesRL.UpdateNote(notesUpdateModel, NotesID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
 
     }
--- RepositoryLayer/Services/NotesRL.cs
+++ RepositoryLayer/Services/NotesRL.cs
@@ -5,8 +5,8 @@ using RepositoryLayer.Entities;
 using RepositoryLayer.Interfaces;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text;
 using System;
+using System.Data;
 
 namespace RepositoryLayer.Services
 {
@@ -60,10 +60,171 @@ namespace RepositoryLayer.Services
 
 
 
-        //public IEnumerable<Notes> GetAllNotes(long UserId)
-        //{
-        //    return FundooContext.NotesTable.Where(x => x.UserId == UserId).ToList();
-        //}
+        public IEnumerable<Notes> GetAllNotes(long UserId)
+        {
+            return fundooContext.NotesTable.Where(x => x.UserId == UserId).ToList();
+        }
+
+
+        public string UpdateNote(NotesModel notesUpdateModel, long NotesID)
+        {
+            try
+            {
+                var update = fundooContext.NotesTable.Where(X => X.NotesId == NotesID).FirstOrDefault();
+                if (update != null && update.NotesId == NotesID)
+                {
+                    update.Title = notesUpdateModel.Title;
+                    update.Body = notesUpdateModel.Body;
+                    update.ModifiedTime = DateTime.Now;
+                    update.Color = notesUpdateModel.Color;
+                    update.Image = notesUpdateModel.Image;
+
+                    this.fundooContext.SaveChanges();
+                    return "Note is Modified";
+                }
+                else
+                {
+                    return " Not Modified";
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string ArchieveNotes(long NotesID)
+        {
+            try
+            {
+                var archive = this.fundooContext.NotesTable.Where(Y => Y.NotesId == NotesID).FirstOrDefault();
+                if (archive.Archieve == true)
+                {
+                    archive.Archieve = false;
+                    this.fundooContext.SaveChanges();
+                    return "note is archieved";
+                }
+
+
+                else
+                {
+                    archive.Archieve = true;
+                    this.fundooContext.SaveChanges();
+                    return "note is now unarchieved";
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool DeleteNotes(long NotesID)
+        {
+            try
+            {
+                var notecheck = this.fundooContext.NotesTable.Where(x => x.NotesId == NotesID).FirstOrDefault();
+                this.fundooContext.NotesTable.Remove(notecheck);
+                int result = this.fundooContext.SaveChanges();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+        public string Pinned(long NotesID)
+        {
+            try
+            {
+                var pinned = this.fundooContext.NotesTable.Where(s => s.NotesId == NotesID).FirstOrDefault();
+                if (pinned.IsPinned == true)
+                {
+                    pinned.IsPinned = false;
+                    this.fundooContext.SaveChanges();
+                    return "note pinned";
+                }
+
+
+                else
+                {
+                    pinned.IsPinned = true;
+                    this.fundooContext.SaveChanges();
+                    return "note is now unpinned";
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public string TrashNotes(long NotesID)
+        {
+            try
+            {
+                var trashed = this.fundooContext.NotesTable.Where(s => s.NotesId == NotesID).FirstOrDefault();
+                if (trashed.Delete == true)
+                {
+                    trashed.Delete = false;
+                    this.fundooContext.SaveChanges();
+                    return "notes recoverd";
+                }
+
+
+                else
+                {
+                    trashed.Delete = true;
+                    this.fundooContext.SaveChanges();
+                    return "note is in trashed";
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public string Notescolor(long NotesID, string color)
+        {
+            try
+            {
+                var addcolor = this.fundooContext.NotesTable.Where(x => x.NotesId == NotesID).FirstOrDefault();
+                if (addcolor.Color == null)
+                {
+                    addcolor.Color = color;
+                    this.fundooContext.SaveChanges();
+                    return "color is added";
+                }
+                else
+                {
+                    return "color is already added";
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
     }
 
     
